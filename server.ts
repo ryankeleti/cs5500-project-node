0faa738b2642ef8041f7978e277db7ea05cfc537
Ryan Keleti <github@ryankeleti.com>
2022-03-30 14:14:41 -0400

    Clean up starter code

--- server.ts
+++ server.ts
@@ -11,13 +11,12 @@
  * service
  */
 import express, {Request, Response} from 'express';
-import CourseController from "./controllers/CourseController";
+import mongoose from "mongoose";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
 import SessionController from "./controllers/SessionController";
 import AuthenticationController from "./controllers/AuthenticationController";
-import mongoose from "mongoose";
 import GroupController from "./controllers/GroupController";
 const cors = require("cors");
 const session = require("express-session");
@@ -29,7 +28,8 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const HOST = "cluster0.m8jeh.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+// connect to the database
 mongoose.connect(connectionString);
 
 const app = express();
@@ -59,7 +59,6 @@ app.get('/', (req: Request, res: Response) =>
     res.send('Welcome!'));
 
 // create RESTful Web service API
-const courseController = new CourseController(app);
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
 const likesController = LikeController.getInstance(app);
